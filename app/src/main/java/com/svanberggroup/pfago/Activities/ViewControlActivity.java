d59c840cff97ad6791fde504bc0def961c829ae6
Glenn Svanberg <signeratsvanberg@gmail.com>
2020-04-22 14:36:46 +0200

    generate cards programmatically

--- app/src/main/java/com/svanberggroup/pfago/Activities/ViewControlActivity.java
+++ app/src/main/java/com/svanberggroup/pfago/Activities/ViewControlActivity.java
@@ -21,7 +21,6 @@ import com.svanberggroup.pfago.Models.Vehicle;
 import com.svanberggroup.pfago.R;
 import com.svanberggroup.pfago.Repository.ControlRepository;
 
-import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -30,8 +29,8 @@ import java.util.List;
 public class ViewControlActivity extends AppCompatActivity {
     private Control control;
     private TextView headerLeft, headerRight, truckText, trailerText, carrierTextLeft, carrierTextRight, senderText, receiverText;
-    private TextView driverText, passengerText, cargoTextLeft, cargoTextRight, goodsDeclarationLeft, goodsDeclarationRight, writtenInstructionsLeft, writtenInstructionsRight;
-    private LinearLayout transportDocumentRows, transportRows;
+    private TextView driverText, passengerText, cargoTextLeft, cargoTextRight;
+    private LinearLayout transportDocumentRows, transportRows, cardsLinearLayout;
 
     private LayoutInflater layoutInflater;
     @Override
@@ -43,6 +42,8 @@ public class ViewControlActivity extends AppCompatActivity {
         ControlRepository repo = ControlRepository.get();
         layoutInflater = (LayoutInflater) getApplicationContext().getSystemService((Context.LAYOUT_INFLATER_SERVICE));
         control = repo.getControlById(id);
+
+        cardsLinearLayout = findViewById(R.id.cards_linear_layout);
 
         headerLeft = findViewById(R.id.control_card_body_left);
         headerRight = findViewById(R.id.control_card_body_right);
@@ -62,30 +63,40 @@ public class ViewControlActivity extends AppCompatActivity {
         transportDocumentRows = findViewById(R.id.transport_document_rows);
         transportRows = findViewById(R.id.transport_rows);
 
+
         setTextFields();
 
     }
     private void setTextFields() {
+        List<View> cards = new ArrayList<>();
 
-        setHeader();
-
-        setVehicleText(control.getTruck(), truckText);
-        setVehicleText(control.getTrailer(), trailerText);
-        setCarrierText();
-
-        setTransporterText(control.getDriver(), "Förare", driverText);
-        setTransporterText(control.getPassenger(), "Besättning", passengerText);
-
-        setTransportLocationText(control.getSender(), true, senderText);
-        setTransportLocationText(control.getReceiver(), false, receiverText);
-        setCargo();
+        setHeaderCard(addCardView(cards));
+        setVehicleCard(addCardView(cards));
+        setTransporterCard(addTransporterCardView(cards));
+        setDestinationsCard(addCardView(cards));
+        setCargoCard(addCardView(cards));
+        //setTdCard();
+        displayViews(cardsLinearLayout, cards);
 
         setTdRows(transportDocumentRows, control.getTdRows());
         setTRows(transportRows, control.getTRows());
     }
+    private TextView cardLeft(View card) {
+        return card.findViewById(R.id.card_body_left);
+    }
 
+    private TextView cardRight(View card) {
+        return card.findViewById(R.id.card_body_right);
+    }
 
-    private void setHeader() {
+    private TextView cardTitle(View card) {
+        return card.findViewById(R.id.card_title);
+    }
+
+    private void setHeaderCard(View card) {
+
+        setText(cardTitle(card), "Kontroll");
+
         StringBuilder str = new StringBuilder();
         SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
 
@@ -95,7 +106,7 @@ public class ViewControlActivity extends AppCompatActivity {
         str.append(line("Datum:", date));
         str.append(line("Plats:", place));
         str.append(line("Platstyp:", getString(control.getLocationType().label)));
-        setText(headerLeft, str.toString());
+        setText(cardLeft(card), str.toString());
 
         str = new StringBuilder();
         formatter = new SimpleDateFormat("HH:mm");
@@ -106,30 +117,44 @@ public class ViewControlActivity extends AppCompatActivity {
         str.append(line("Start:", startDate));
         str.append(line("Slut:", endDate));
 
-        setText(headerRight, str.toString());
+        setText(cardRight(card), str.toString());
     }
-
-    private void setVehicleText(Vehicle vehicle, TextView textView) {
+    private void setVehicleCard(View card) {
+        setText(cardTitle(card), "Fordon");
+        setText(cardLeft(card), vehicleText(control.getTruck()));
+        setText(cardRight(card), vehicleText(control.getTrailer()));
+    }
+    private String vehicleText(Vehicle vehicle) {
         StringBuilder str = new StringBuilder();
 
         str.append(line("", getString(vehicle.getVehicleType().label)));
         str.append(line("RegNr:", vehicle.getRegNr()));
         str.append(line("Nationalitet:", vehicle.getNationality()));
 
-        setText(textView, str.toString());
+        return str.toString();
     }
+    private void setTransporterCard(View card){
+        setText(cardTitle(card), "Transportör");
+        setCarrier(card);
+        TextView driverTextView = card.findViewById(R.id.driver_text);
+        TextView passengerTextView = card.findViewById(R.id.passenger_text);
 
-    private void setCarrierText(){
+        setText(driverTextView, setTransporterText(control.getDriver(), "Förare"));
+        setText(passengerTextView, setTransporterText((control.getPassenger()), "Passagerare"));
+    }
+    private void setCarrier(View card){
         StringBuilder str = new StringBuilder();
+        Transporter carrier = control.getCarrier();
+        TextView left = card.findViewById(R.id.carrier_text_left);
+        TextView right = card.findViewById(R.id.carrier_text_right);
 
-        str.append(line("Företag:", control.getCarrier().getName()));
-        str.append(line("Tel:", control.getCarrier().getPhone()));
+        str.append(line("Företag:", carrier.getName()));
+        str.append(line("Tel:", carrier.getPhone()));
 
-        setText(carrierTextLeft, str.toString());
-        setText(carrierTextRight, address(control.getCarrier()));
+        setText(left, str.toString());
+        setText(right, address(carrier));
     }
-
-    private void setTransporterText(Transporter transporter, String title, TextView textView) {
+    private String setTransporterText(Transporter transporter, String title) {
         StringBuilder str = new StringBuilder();
 
         str.append(line("", title));
@@ -137,10 +162,14 @@ public class ViewControlActivity extends AppCompatActivity {
         str.append(line("Tel:", transporter.getPhone()));
         str.append(address(transporter));
 
-        setText(textView, str.toString());
+        return str.toString();
     }
-
-    private void setTransportLocationText(TransportLocation location, boolean isSender, TextView textView) {
+    private void setDestinationsCard(View card) {
+        setText(cardTitle(card), "Destinationer");
+        setText(cardLeft(card), destinationText(control.getSender(), true));
+        setText(cardRight(card), destinationText(control.getReceiver(), false));
+    }
+    private String destinationText(TransportLocation location, boolean isSender) {
         StringBuilder str = new StringBuilder();
         if(isSender) {
             str.append(line("", "Avsändare"));
@@ -153,70 +182,82 @@ public class ViewControlActivity extends AppCompatActivity {
         }
         str.append(line("Tel:", location.getPhone()));
 
-        setText(textView,str.toString());
+        return str.toString();
     }
 
-    private void setCargo() {
+
+
+    private void setCargoCard(View card){
         StringBuilder str = new StringBuilder();
         Quantity qty = control.getQuantity();
+        setText(cardTitle(card), "Gods");
+
         str.append(line("Mängd:", qty.getQuantity() + getString(qty.getQuantityType().label)));
         str.append(line("Standard:", getString(qty.getPackagingStandard().label)));
         str.append(line("Värdeberäknad mängd:", "" + control.getValueQuantity()));
-        String exceeded;
-        if(control.isValueQuantityExceeded()) {
-            exceeded = "Ja";
-        } else {
-            exceeded = "Nej";
-        }
-        str.append(line("Överskriden:", exceeded));
-        setText(cargoTextLeft, str.toString());
+
+        str.append(line("Överskriden:", control.isValueQuantityExceeded() ? "Ja" : "Nej"));
+        setText(cardLeft(card), str.toString());
 
         str = new StringBuilder();
         str.append(line("Transport med:", getString(control.getTransportType().label)));
         str.append(line("Transport enligt:", getString(control.getTransportStandard().label)));
-        setText(cargoTextRight, str.toString());
+        setText(cardRight(card), str.toString());
     }
+
 
     private void setTdRows(LinearLayout layout, TransportDocumentRows tdRows) {
         ArrayList<View> views = new ArrayList<>();
 
         String declaration = Html.fromHtml(line("", getString(tdRows.getDeclaration().label))).toString();
-        setControlRow(tdRows.getGoodsDeclarationRow(), addView(layout,views), "13. Godsdeklaration",declaration);
-        setControlRow(tdRows.getWrittenInstructionsRow(), addView(layout,views), "14. Skriftliga instruktioner","");
-        setControlRow(tdRows.getApprovalRow(), addView(layout,views), "15." + getString(tdRows.getApproval().label),"");
-        setControlRow(tdRows.getApprovalCertificateRow(), addView(layout,views), "16. Godkännandecertifikat","");
-        setControlRow(tdRows.getDriverCertificationRow(), addView(layout,views), "17.1. Förarintyg (ADR 8.2.1, 8.2.2)","");
-        setControlRow(tdRows.getOtherADRTrainingRow(), addView(layout,views), "17.2. Annan ADR-utbildning","");
+        setControlRow(tdRows.getGoodsDeclarationRow(), addRowView(layout,views), "13. Godsdeklaration",declaration);
+        setControlRow(tdRows.getWrittenInstructionsRow(), addRowView(layout,views), "14. Skriftliga instruktioner","");
+        setControlRow(tdRows.getApprovalRow(), addRowView(layout,views), "15." + getString(tdRows.getApproval().label),"");
+        setControlRow(tdRows.getApprovalCertificateRow(), addRowView(layout,views), "16. Godkännandecertifikat","");
+        setControlRow(tdRows.getDriverCertificationRow(), addRowView(layout,views), "17.1. Förarintyg (ADR 8.2.1, 8.2.2)","");
+        setControlRow(tdRows.getOtherADRTrainingRow(), addRowView(layout,views), "17.2. Annan ADR-utbildning","");
         displayViews(layout, views);
     }
     private void setTRows(LinearLayout layout, TransportRows tRows) {
         ArrayList<View> views = new ArrayList<>();
 
-        setControlRow(tRows.getRow18(), addView(layout,views), "18. Gods tillåtet för transport","");
-        setControlRow(tRows.getRow19(), addView(layout,views), "19. Fordonet godkänt för det transporterade godset","");
-        setControlRow(tRows.getRow20(), addView(layout,views), "20. Bestämmelser för transportsätt","");
-        setControlRow(tRows.getRow21(), addView(layout,views), "21. Förbud mot samlastning","");
-        setControlRow(tRows.getRow22_1(), addView(layout,views), "22.1. Hantering","");
-        setControlRow(tRows.getRow22_2(), addView(layout,views), "22.2. Lastning/Stuvning ","");
-        setControlRow(tRows.getRow22_3(), addView(layout,views), "22.3. Lastsäkring","");
-        setControlRow(tRows.getRow23_1(), addView(layout,views), "23.1. Läckage","");
-        setControlRow(tRows.getRow23_2(), addView(layout,views), "23.2. Skador på kolli/fordon","");
-        setControlRow(tRows.getRow24(), addView(layout,views), "24. Godkännandemärkning av kolli/tankar (ADR kapitel 6)","");
-        setControlRow(tRows.getRow25_1(), addView(layout,views), "25.1. Märkning av kolli (5.2.1)","");
-        setControlRow(tRows.getRow25_2(), addView(layout,views), "25.2. Ettikering av kolli (5.2.2","");
-        setControlRow(tRows.getRow26(), addView(layout,views), "26. Storetiketter (5.3.1","");
-        setControlRow(tRows.getRow27(), addView(layout,views), "27. Skyltning/märkning av fordon/transporthenhet","");
-        setControlRow(tRows.getRow28_1(), addView(layout,views), "28.1. Stoppklots","");
-        setControlRow(tRows.getRow28_2(), addView(layout,views), "28.2. Varningsanordning","");
-        setControlRow(tRows.getRow28_3(), addView(layout,views), "28.3. Varningsväst/ögonskydd/handskar","");
-        setControlRow(tRows.getRow28_4(), addView(layout,views), "28.4. Bärbar ljuskälla","");
-        setControlRow(tRows.getRow29(), addView(layout,views), "29. Godsspecifik utrustning (ADR 8.1.5.2/8.1.5.3)","");
-        setControlRow(tRows.getRow31(), addView(layout,views), "31. Brandsläckare (ADR 8.1.4.1/8.1.4.2/10.6)","");
+        setControlRow(tRows.getRow18(), addRowView(layout,views), "18. Gods tillåtet för transport","");
+        setControlRow(tRows.getRow19(), addRowView(layout,views), "19. Fordonet godkänt för det transporterade godset","");
+        setControlRow(tRows.getRow20(), addRowView(layout,views), "20. Bestämmelser för transportsätt","");
+        setControlRow(tRows.getRow21(), addRowView(layout,views), "21. Förbud mot samlastning","");
+        setControlRow(tRows.getRow22_1(), addRowView(layout,views), "22.1. Hantering","");
+        setControlRow(tRows.getRow22_2(), addRowView(layout,views), "22.2. Lastning/Stuvning ","");
+        setControlRow(tRows.getRow22_3(), addRowView(layout,views), "22.3. Lastsäkring","");
+        setControlRow(tRows.getRow23_1(), addRowView(layout,views), "23.1. Läckage","");
+        setControlRow(tRows.getRow23_2(), addRowView(layout,views), "23.2. Skador på kolli/fordon","");
+        setControlRow(tRows.getRow24(), addRowView(layout,views), "24. Godkännandemärkning av kolli/tankar (ADR kapitel 6)","");
+        setControlRow(tRows.getRow25_1(), addRowView(layout,views), "25.1. Märkning av kolli (5.2.1)","");
+        setControlRow(tRows.getRow25_2(), addRowView(layout,views), "25.2. Ettikering av kolli (5.2.2","");
+        setControlRow(tRows.getRow26(), addRowView(layout,views), "26. Storetiketter (5.3.1","");
+        setControlRow(tRows.getRow27(), addRowView(layout,views), "27. Skyltning/märkning av fordon/transporthenhet","");
+        setControlRow(tRows.getRow28_1(), addRowView(layout,views), "28.1. Stoppklots","");
+        setControlRow(tRows.getRow28_2(), addRowView(layout,views), "28.2. Varningsanordning","");
+        setControlRow(tRows.getRow28_3(), addRowView(layout,views), "28.3. Varningsväst/ögonskydd/handskar","");
+        setControlRow(tRows.getRow28_4(), addRowView(layout,views), "28.4. Bärbar ljuskälla","");
+        setControlRow(tRows.getRow29(), addRowView(layout,views), "29. Godsspecifik utrustning (ADR 8.1.5.2/8.1.5.3)","");
+        setControlRow(tRows.getRow31(), addRowView(layout,views), "31. Brandsläckare (ADR 8.1.4.1/8.1.4.2/10.6)","");
         displayViews(layout, views);
 
     }
 
-    private View addView(LinearLayout linearLayout, List<View> views) {
+    private View addCardView(List<View> views){
+        View view = layoutInflater.inflate(R.layout.view_control_card, cardsLinearLayout,false);
+        views.add(view);
+        return view;
+    }
+
+    private View addTransporterCardView(List<View> views){
+        View view = layoutInflater.inflate(R.layout.view_control_transporter_card, cardsLinearLayout,false);
+        views.add(view);
+        return view;
+    }
+
+    private View addRowView(LinearLayout linearLayout, List<View> views) {
         View view;
         if(views.size() == 0) {
             view = layoutInflater.inflate(R.layout.view_control_row_first, linearLayout,false);
