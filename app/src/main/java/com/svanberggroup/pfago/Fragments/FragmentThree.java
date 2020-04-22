02cee2036a24d01c64425a233a52c29ca31a75dc
Robin Törnqvist <robintornqvist92@gmail.com>
2020-04-22 12:24:24 +0200

    Added layout for fragment_four

--- app/src/main/java/com/svanberggroup/pfago/Fragments/FragmentThree.java
+++ app/src/main/java/com/svanberggroup/pfago/Fragments/FragmentThree.java
@@ -45,7 +45,6 @@ public class FragmentThree extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_three, container, false);
-
         return  view;
     }
 
