9bd0cde13a6df8231c38bfd8fbe89bcfde66f9c1
Robin Törnqvist <robintornqvist92@gmail.com>
2020-04-27 16:55:06 +0200

    Fragment Eight, needs work

---
+++ app/src/main/java/com/svanberggroup/pfago/Fragments/FragmentEight.java
@@ -0,0 +1,120 @@
+package com.svanberggroup.pfago.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.svanberggroup.pfago.Models.Control;
+import com.svanberggroup.pfago.Models.ControlRow;
+import com.svanberggroup.pfago.Models.TransportRows;
+import com.svanberggroup.pfago.R;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class FragmentEight extends Fragment {
+
+
+
+    private static final String NEW_CONTROL = "new_control";
+
+    ArrayList<ControlRow> otherList = new ArrayList<>();
+    private View child;
+
+    private Control control;
+    private TransportRows tRows;
+    private ArrayList<View> views = new ArrayList<>();
+    private LinearLayout otherLinerLayout;
+
+    public FragmentEight() {
+    }
+
+    public static FragmentEight newInstance(Control control) {
+        FragmentEight fragment = new FragmentEight();
+        Bundle args = new Bundle();
+        args.putSerializable(NEW_CONTROL, (Serializable) control);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        control = (Control) getArguments().getSerializable(NEW_CONTROL);
+
+        if (control.getTRows() != null) {
+            tRows = control.getTRows();
+        } else {
+            tRows = new TransportRows();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_eight, container, false);
+
+        otherLinerLayout = (LinearLayout) view.findViewById(R.id.otherLinearLayout);
+        child = inflater.inflate(R.layout.control_layout, null);
+        View child1 = inflater.inflate(R.layout.control_layout, null);
+        View child2 = inflater.inflate(R.layout.control_layout, null);
+        views.add(child);
+        views.add(child1);
+        views.add(child2);
+
+        displayViews();
+
+        return  view;
+    }
+
+
+
+    private void displayViews() {
+        for (View v: views) {
+            otherLinerLayout.addView(v);
+        }
+    }
+
+    private void addRadioButtonCheckedListner() {
+        for (View v: views) {
+
+            tRows = control.getTRows();
+            ControlRow cRow = new ControlRow("janne");
+            RadioGroup group = v.findViewById(R.id.radioGroupABC);
+
+            group.setOnCheckedChangeListener((group1, checkedId) -> {
+                if (v == child) {
+                    switch (checkedId) {
+                        case R.id.radioButtonControlled:
+                            cRow.setField(ControlRow.Field.Controlled);
+                            tRows.addRow40(cRow);
+                            break;
+                        case R.id.radioButtonNotApproved:
+                            cRow.setField(ControlRow.Field.BreakingTheLaw);
+                            tRows.addRow40(cRow);
+                        case R.id.radioButtonNotApplicible:
+                            cRow.setField(ControlRow.Field.NotApplicable);
+                            tRows.addRow40(cRow);
+                    }
+                }
+            });
+        }
+    }
+
+
+    @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
