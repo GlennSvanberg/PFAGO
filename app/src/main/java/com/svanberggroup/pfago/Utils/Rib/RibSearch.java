d3ccf9636b07e80f76b3ae5ad07e4d98781f7815
HerrSven <63953357+HerrSven@users.noreply.github.com>
2020-04-29 11:46:20 +0200

    Handle edge case: UN RIB:search

--- app/src/main/java/com/svanberggroup/pfago/Utils/Rib/RibSearch.java
+++ app/src/main/java/com/svanberggroup/pfago/Utils/Rib/RibSearch.java
@@ -1,5 +1,7 @@
 package com.svanberggroup.pfago.Utils.Rib;
 
+import android.os.AsyncTask;
+
 import com.svanberggroup.pfago.Models.RibSearchResult;
 import com.svanberggroup.pfago.Utils.Rib.Constants.HtmlAttr;
 import com.svanberggroup.pfago.Utils.Rib.Constants.RibUrl;
@@ -8,18 +10,27 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class RibSearch {
 
     public ArrayList<RibSearchResult> runRequest(String query) {
+        String queryNormalised = "";
+
+        if (query.toLowerCase().contains("un")) {
+            queryNormalised = query.toLowerCase().replace("un", "").trim();
+        } else {
+            queryNormalised = query.trim();
+        }
+
         Document response = null;
         try {
-            if (query.trim().matches("^\\d+$")) {
-                response = Jsoup.connect(RibUrl.BASE + query).get();
+            if (queryNormalised.matches("^\\d+$")) {
+                response = Jsoup.connect(RibUrl.BASE + queryNormalised).get();
             } else
-                response = Jsoup.connect(RibUrl.BASE + query + "/").get();
+                response = Jsoup.connect(RibUrl.BASE + queryNormalised + "/").get();
         } catch (IOException e) {
             e.printStackTrace();
             return null;
